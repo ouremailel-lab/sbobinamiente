7e9dc5ad42f5530c5f5fc33626cf1295c4cbe4b8
Elisai Iannone <iannonelsia@gmail.com>
2026-02-22 19:49:44 +0100

    Aggiungi script checkSite.js per controllo stato sito

---
+++ checkSite.js
@@ -0,0 +1,13 @@
+const https = require('https');
+
+const url = 'https://sbobinamente.it';
+
+https.get(url, (res) => {
+  if (res.statusCode === 200) {
+    console.log('Il sito è ONLINE!');
+  } else {
+    console.log(`Il sito risponde ma con status: ${res.statusCode}`);
+  }
+}).on('error', (e) => {
+  console.log('Il sito NON è raggiungibile:', e.message);
+});
